0ac853628bfb7c222b0ec676b663ff6395dc3b36
rronaldo007 <rukundoronaldo4@gmail.com>
2026-07-18 09:59:53 +0200

    EDC01-7 chore(pm2): déploiement 3 instances cluster + logs + limite mémoire
    - instances: 3 en exec_mode cluster (déclaré dans le fichier: -i 3 en CLI est ignoré avec un config file)
    - error_file vers /logs/err.log, max_memory_restart à 200M
    - lancement: pm2 start ecosystem.config.js --env production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+      instances: 3,
+      exec_mode: "cluster",
       error_file: "/logs/err.log",
       max_memory_restart: "200M",
       env_production: {
