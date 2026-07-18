f0d7b95fcdfa6b7c9ae2e4da3675fc49c8d3ad60
rronaldo007 <rukundoronaldo4@gmail.com>
2026-07-18 09:55:32 +0200

    EDC01-7 chore(pm2): config de déploiement (error_file + max_memory_restart 200M)
    - error_file vers /logs/err.log
    - max_memory_restart à 200M
    - lancement 3 instances: pm2 start ecosystem.config.js -i 3 --env production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+      error_file: "/logs/err.log",
+      max_memory_restart: "200M",
       env_production: {
         NODE_ENV: "production",
       },
