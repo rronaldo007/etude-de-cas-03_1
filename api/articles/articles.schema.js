bb662b61228e5fe4c193f81abbec4386b7419be2
rronaldo007 <rukundoronaldo4@gmail.com>
2026-07-18 08:23:04 +0200

    EDC01-3 feat(articles): ajout du statut draft/published (enum Mongoose)

--- api/articles/articles.schema.js
+++ api/articles/articles.schema.js
@@ -3,6 +3,14 @@ const { Schema, model } = require("mongoose");
 const articleSchema = Schema({
   title: String,
   content: String,
+  status: {
+    type: String,
+    enum: {
+      values: ["draft", "published"],
+      message: "{VALUE} inconnue",
+    },
+    default: "draft",
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
